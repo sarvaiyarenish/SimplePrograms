8a15ca8d792e6882718107a6d1a476e358d820c8
Renish <Renish@RENISH>
2025-07-04 11:32:42 +0530

    Add project files.

---
+++ OddEven.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimplePrograms
+{
+    internal class OddEven
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Number:");
+            int a = int.Parse(Console.ReadLine());
+
+            if(a % 2 == 0)
+            {
+                Console.WriteLine("Given Number is Even");
+            }
+            else
+            {
+                Console.WriteLine("Given Number is Odd");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimplePrograms
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Enter Num1:");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Num2:");
+            int b = int.Parse(Console.ReadLine());
+
+            int c = a + b;
+            Console.WriteLine("Sum is:" + c);
+
+        }
+    }
+}
